a1ccd4404e31234e937bbfa2e48a94fc385c1f55
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-09 13:21:09 +0100

    fix(schema): remove unstable api

--- packages/schema/src/_api/withDefaultConstructorField.ts
+++ packages/schema/src/_api/withDefaultConstructorField.ts
@@ -4,12 +4,11 @@ import { pipe } from "@effect-ts/system/Function"
 import * as S from "../_schema"
 import * as Constructor from "../Constructor"
 
-export type OptionalKey<ConstructorInput, Key extends keyof ConstructorInput> = {
-  [K in Exclude<keyof ConstructorInput, Key>]: ConstructorInput[K]
-} &
-  {
-    [K in Key]?: ConstructorInput[Key]
-  }
+export type OptionalKey<ConstructorInput, Key extends keyof ConstructorInput> = Omit<
+  ConstructorInput,
+  Key
+> &
+  Partial<Pick<ConstructorInput, Key>>
 
 export function withDefaultConstructorField<
   ConstructorInput,
@@ -52,59 +51,6 @@ export function withDefaultConstructorField<
       self,
       S.constructor((u: any) =>
         constructSelf(typeof u[key] !== "undefined" ? u : { ...u, [key]: value() })
-      )
-    )
-  }
-}
-
-export function withDefaultConstructorFields<
-  ConstructorInput,
-  Changes extends {
-    [P in keyof ConstructorInput]?: Lazy<ConstructorInput[P]>
-  }
->(kvs: Changes) {
-  return <
-    ParserInput,
-    ParserError,
-    ParsedShape,
-    ConstructorError,
-    ConstructedShape extends ParsedShape,
-    Encoded,
-    Api
-  >(
-    self: S.Schema<
-      ParserInput,
-      ParserError,
-      ParsedShape,
-      ConstructorInput,
-      ConstructorError,
-      ConstructedShape,
-      Encoded,
-      Api
-    >
-  ): S.Schema<
-    ParserInput,
-    ParserError,
-    ParsedShape,
-    OptionalKey<ConstructorInput, keyof Changes & keyof ConstructorInput>,
-    ConstructorError,
-    ConstructedShape,
-    Encoded,
-    Api
-  > => {
-    const constructSelf = Constructor.for(self)
-    return pipe(
-      self,
-      S.constructor((u: any) =>
-        constructSelf({
-          ...u,
-          ...Object.keys(kvs).reduce((prev, cur) => {
-            if (typeof u[cur] === "undefined") {
-              prev[cur] = kvs[cur]()
-            }
-            return prev
-          }, {} as any)
-        } as any)
       )
     )
   }
--- packages/schema/test/schemed.test.ts
+++ packages/schema/test/schemed.test.ts
@@ -1,5 +1,5 @@
 import * as T from "@effect-ts/core/Effect"
-import { constant, pipe } from "@effect-ts/core/Function"
+import { pipe } from "@effect-ts/core/Function"
 import * as FC from "fast-check"
 
 import * as MO from "../src"
@@ -14,7 +14,7 @@ export class Person extends MO.Schemed(
       lastName: MO.string
     }),
     MO.tag("Person"),
-    MO.withDefaultConstructorFields({ firstName: constant("Mike") })
+    MO.withDefaultConstructorField("firstName", () => "Mike")
   )
 ) {
   static Model = MO.schema(Person)
