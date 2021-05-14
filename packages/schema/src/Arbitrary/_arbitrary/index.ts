a1ada3f0843cf014e20ba5f7068c8232e6492dcd
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-14 15:20:24 +0100

    feat(schema): recursion safety

--- packages/schema/src/Arbitrary/_arbitrary/index.ts
+++ packages/schema/src/Arbitrary/_arbitrary/index.ts
@@ -8,35 +8,22 @@ import { hasContinuation, SchemaContinuationSymbol } from "../../_schema"
 
 export type Gen<T> = { (_: typeof fc): fc.Arbitrary<T> }
 
-const interpreterCache = new WeakMap()
-const interpretedCache = new WeakMap()
-
-export const interpreters: ((schema: S.SchemaAny) => O.Option<Gen<unknown>>)[] = [
-  O.partial((miss) => (schema: S.SchemaAny): Gen<unknown> => {
+export const interpreters: ((schema: S.SchemaAny) => O.Option<() => Gen<unknown>>)[] = [
+  O.partial((miss) => (schema: S.SchemaAny): (() => Gen<unknown>) => {
     if (schema instanceof S.SchemaRecursive) {
-      if (interpreterCache.has(schema)) {
-        return interpreterCache.get(schema)
-      }
-      const parser = (_: typeof fc) => {
-        if (interpretedCache.has(schema)) {
-          return interpretedCache.get(schema)
-        }
-        const e = for_(schema.self(schema))(_)
-        interpretedCache.set(schema, e)
-        return e
-      }
-      interpreterCache.set(schema, parser)
-      return parser
+      return () => for_(schema.self(schema))
     }
     if (schema instanceof S.SchemaIdentity) {
-      return (_) => _.anything().filter(schema.guard)
+      return () => (_) => _.anything().filter(schema.guard)
     }
     if (schema instanceof S.SchemaArbitrary) {
-      return schema.arbitrary
+      return () => schema.arbitrary
     }
     if (schema instanceof S.SchemaRefinement) {
-      const self = for_(schema.self)
-      return (_) => self(_).filter(schema.refinement)
+      return () => {
+        const self = for_(schema.self)
+        return (_) => self(_).filter(schema.refinement)
+      }
     }
     return miss()
   })
@@ -69,12 +56,25 @@ function for_<
   for (const interpreter of interpreters) {
     const _ = interpreter(schema)
     if (_._tag === "Some") {
-      cache.set(schema, _.value)
-      return _.value as Gen<ParsedShape>
+      let x: Gen<unknown>
+      const arb: Gen<unknown> = (__) => {
+        if (!x) {
+          x = _.value()
+        }
+        return x(__)
+      }
+      cache.set(schema, arb)
+      return arb as Gen<ParsedShape>
     }
   }
   if (hasContinuation(schema)) {
-    const arb = for_(schema[SchemaContinuationSymbol])
+    let x: Gen<unknown>
+    const arb: Gen<unknown> = (__) => {
+      if (!x) {
+        x = for_(schema[SchemaContinuationSymbol])
+      }
+      return x(__)
+    }
     cache.set(schema, arb)
     return arb as Gen<ParsedShape>
   }
--- packages/schema/src/Encoder/_encoder/index.ts
+++ packages/schema/src/Encoder/_encoder/index.ts
@@ -8,42 +8,27 @@ export type Encoder<Output, Encoded> = {
   (u: Output): Encoded
 }
 
-const interpreterCache = new WeakMap()
-const interpretedCache = new WeakMap()
-
 export const interpreters: ((
   schema: S.SchemaAny
-) => O.Option<Encoder<unknown, unknown>>)[] = [
-  O.partial((miss) => (schema: S.SchemaAny): Encoder<unknown, unknown> => {
+) => O.Option<() => Encoder<unknown, unknown>>)[] = [
+  O.partial((miss) => (schema: S.SchemaAny): (() => Encoder<unknown, unknown>) => {
     if (schema instanceof S.SchemaRecursive) {
-      if (interpreterCache.has(schema)) {
-        return interpreterCache.get(schema)
-      }
-      const parser = (u: unknown) => {
-        if (interpretedCache.has(schema)) {
-          return interpretedCache.get(schema)(u)
-        }
-        const e = encoderFor(schema.self(schema))
-        interpretedCache.set(schema, e)
-        return e(u)
-      }
-      interpreterCache.set(schema, parser)
-      return parser
+      return () => encoderFor(schema.self(schema))
     }
     if (schema instanceof S.SchemaIdentity) {
-      return (_) => _
+      return () => (_) => _
     }
     if (schema instanceof S.SchemaPipe) {
       const encodeSelf = encoderFor(schema.that)
       const encodeThat = encoderFor(schema.self)
 
-      return (_) => encodeThat(encodeSelf(_))
+      return () => (_) => encodeThat(encodeSelf(_))
     }
     if (schema instanceof S.SchemaRefinement) {
-      return encoderFor(schema.self)
+      return () => encoderFor(schema.self)
     }
     if (schema instanceof S.SchemaEncoder) {
-      return schema.encoder
+      return () => schema.encoder
     }
     return miss()
   })
@@ -76,12 +61,25 @@ function encoderFor<
   for (const interpreter of interpreters) {
     const _ = interpreter(schema)
     if (_._tag === "Some") {
-      cache.set(schema, _.value)
-      return _.value as Encoder<ParsedShape, Encoded>
+      let x: Encoder<unknown, unknown>
+      const encoder: Encoder<unknown, unknown> = (u) => {
+        if (!x) {
+          x = _.value()
+        }
+        return x(u)
+      }
+      cache.set(schema, encoder)
+      return encoder as Encoder<ParsedShape, Encoded>
     }
   }
   if (S.hasContinuation(schema)) {
-    const encoder = encoderFor(schema[S.SchemaContinuationSymbol])
+    let x: Encoder<unknown, unknown>
+    const encoder: Encoder<unknown, unknown> = (u) => {
+      if (!x) {
+        x = encoderFor(schema[S.SchemaContinuationSymbol])
+      }
+      return x(u)
+    }
     cache.set(schema, encoder)
     return encoder as Encoder<ParsedShape, Encoded>
   }
